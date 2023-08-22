f454556b7b5b2f19e20abfd4f11b73289348c639
odbaayaar <chadestlad10@gmai.com>
2023-08-22 06:44:42 -0700

    a

---
+++ testing/src/App.js
@@ -0,0 +1,231 @@
+import './App.css';
+import {AiFillLock} from 'react-icons/ai'
+
+function App() {
+  return (
+    <>
+    <div className="Body">
+      <div className="a">
+        <div className="date">The 25th May 16:03:43</div>
+      </div>
+      <div className="b">
+        <div className="b1">
+          <a>time</a>
+          <a>Nbr</a>
+        </div>
+        <div className="b2">
+          <a>3-Way</a>
+          <a>rest of match</a>
+          <a>Over/Under</a>
+        </div>
+      </div>
+      <div className="c">
+        <div className="time">78'</div>
+        <div className="nbr">13175</div>
+        <div className="name">
+          <div className="name2">
+            <div>Kigezi Homeboyz FC</div>
+            <div className="cup">Uganda / Cup</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>1:1</div>
+          <div>Bul FC</div>
+          </div>
+        </div>
+        <div className='way'><AiFillLock/><AiFillLock/><AiFillLock/></div>
+        <div className='way'><AiFillLock/><AiFillLock/><AiFillLock/></div>
+        <div className='way'><div className='yell'>2.5</div><AiFillLock/><AiFillLock/></div>
+      </div>
+      <div className="d">
+        <div className="time">77'</div>
+        <div className="nbr">13306</div>
+        <div className="name">
+          <div className="name2">
+            <div>Cska Pamir Dushanbe</div>
+            <div className="cup">Tajikstan / Tajik League</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>0:0</div>
+          <div>1️⃣Istaravshan</div>
+          </div>
+        </div>  
+        <div className='way'><div className='white'>2.05</div><div className='white'>1.75</div><div className='white'>14.5</div></div>
+        <div className='way'><div className='white'>2.05</div><div className='white'>1.75</div><div className='white'>14.5</div></div>
+        <div className='way'><div className='yell'>0.5</div><div className='white'>1.75</div><div className='white'>1.80</div></div>
+      </div>
+      <div className="c">
+        <div className="time">74'</div>
+        <div className="nbr">6581</div>
+        <div className="name">
+          <div className="name2">
+            <div>FC Khation</div>
+            <div className="cup">Uganda / Cup</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>0:1</div>
+          <div>FC Faizksnd</div>
+          </div>
+        </div>
+        <div className='way'><div className='white'>15.0</div><div className='white'>4.30</div><div className='white'>1.25</div></div>
+        <div className='way'><div className='white'>3.70</div><div className='white'>1.65</div><div className='white'>5.00</div></div>
+        <div className='way'><div className='yell'>1.5</div><div className='white'>1.97</div><div className='white'>1.73</div></div>
+      </div>
+      <div className="d">
+        <div className="time">71'</div>
+        <div className="nbr">13197</div>
+        <div className="name">
+          <div className="name2">
+            <div>Vipers SC</div>
+            <div className="cup">Uganda / Cup</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>0:0</div>
+          <div>SC Villa</div>
+          </div>
+        </div>
+        <div className='way'><AiFillLock/><AiFillLock/><AiFillLock/></div>
+        <div className='way'><AiFillLock/><AiFillLock/><AiFillLock/></div>
+        <div className='way'><div className='yell'>0.5</div><AiFillLock/><AiFillLock/></div>
+      </div>
+      <div className="c">
+        <div className="time">49'</div>
+        <div className="nbr">13200</div>
+        <div className="name">
+          <div className="name2">
+            <div>ZNK Olimpija Ljubjjana</div>
+            <div className="cup">Uganda / Cup</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>0:0</div>
+          <div>ZNK Pomurje</div>
+          </div>
+        </div>
+        <div className='way'><div className='white'>8.25</div><div className='white'>!3.70</div><div className='white'>1.40</div></div>
+        <div className='way'><div className='white'>8.25</div><div className='white'>!3.70</div><div className='white'>1.40</div></div>
+        <div className='way'><div className='yell'>1.5</div><div className='white'>!1.45</div><div className='white'>2.30</div></div>
+      </div>
+      <div className="d">
+        <div className="time">47'</div>
+        <div className="nbr">13370</div>
+        <div className="name">
+          <div className="name2">
+            <div>FC Samtredia</div>
+            <div className="cup">Georgia</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>1:2</div>
+          <div>FC Samgura Tskaltubo</div>
+          </div>
+        </div>
+        <div className='way'><AiFillLock/><AiFillLock/><AiFillLock/></div>
+        <div className='way'><AiFillLock/><AiFillLock/><AiFillLock/></div>
+        <div className='way'><div className='yell'>4.5</div><AiFillLock/><AiFillLock/></div>
+      </div>
+      <div className="c">
+        <div className="time2">HT</div>
+        <div className="nbr">3941</div>
+        <div className="name">
+          <div className="name2">
+            <div>Red Arrows</div>
+            <div className="cup">Zambia</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>0:0</div>
+          <div>Nikana FC</div>
+          </div>
+        </div>
+        <div className='way'><div className='white'>2.30</div><div className='white'>2.30</div><div className='white'>4.05</div></div>
+        <div className='way'><div className='white'>2.30</div><div className='white'>2.30</div><div className='white'>4.05</div></div>
+        <div className='way'><div className='yell'>1.5</div><div className='white'>2.25</div><div className='white'>1.47</div></div>
+      </div>
+      <div className="d">
+        <div className="time3">17:00</div>
+        <div className="nbr">5022</div>
+        <div className="name">
+          <div className="name2">
+            <div>FK Dukla Banska Bystrica</div>
+            <div className="cup">Uganda / Cup</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>0:0</div>
+          <div>FK Senica</div>
+          </div>
+        </div>
+        <div className='way'><div className='white'>2.68</div><div className='white'>3.50</div><div className='white'>2.43</div></div>
+        <div className='way'><AiFillLock/><AiFillLock/><AiFillLock/></div>
+        <div className='way'><div className='yell'>2.5</div><div className='white'>1.76</div><div className='white'>1.99</div></div>
+      </div>
+      <div className="c">
+        <div className="time3">16:00</div>
+        <div className="nbr">10150</div>
+        <div className="name">
+          <div className="name2">
+            <div>Vijandi JK Tulevic</div>
+            <div className="cup">Uganda / Cup</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>0:0</div>
+          <div>Talinna JK Legion</div>
+          </div>
+        </div>
+        <div className='way'><div className='white'>4.00</div><div className='white'>3.37</div><div className='white'>1.80</div></div>
+        <div className='way'><AiFillLock/><AiFillLock/><AiFillLock/></div>
+        <div className='way'><div className='yell'>2.5</div><div className='white'>1.68</div><div className='white'>2.05</div></div>
+      </div>
+      <div className="d">
+        <div className="time3">18:45</div>
+        <div className="nbr">13175</div>
+        <div className="name">
+          <div className="name2">
+            <div>Hapoel Acre FC</div>
+            <div className="cup">Uganda / Cup</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>0:0</div>
+          <div>Maccabi Ahi Nazareth</div>
+          </div>
+        </div>
+        <div className='way'><div className='white'>2.30</div><div className='white'>2.30</div><div className='white'>4.05</div></div>
+        <div className='way'><AiFillLock/><AiFillLock/><AiFillLock/></div>
+        <div className='way'><div className='yell'>2.5</div><div className='white'>2.07</div><div className='white'>1.67</div></div>
+      </div>
+      <div className="c">
+        <div className="time3">19:00</div>
+        <div className="nbr">4425</div>
+        <div className="name">
+          <div className="name2">
+            <div>Civeria Zvezda</div>
+            <div className="cup">Uganda / Cup</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>0:0</div>
+          <div>FK Parllzan Belgrade</div>
+          </div>
+        </div>
+        <div className='way'><div className='white'>2.30</div><div className='white'>2.30</div><div className='white'>4.05</div></div>
+        <div className='way'><AiFillLock/><AiFillLock/><AiFillLock/></div>
+        <div className='way'><div className='yell'>2.5</div><div className='white'>2.30</div><div className='white'>1.55</div></div>
+      </div>
+      <div className="d">
+        <div className="time3">19:00</div>
+        <div className="nbr">4112</div>
+        <div className="name">
+          <div className="name2">
+            <div>Wadi Degla FC</div>
+            <div className="cup">Egypt</div>
+          </div>
+          <div className="name3">
+          <div style={{"color" : "yellow", "paddingRight" : "15px"}}>0:0</div>
+          <div>Talaea El Galsh SC</div>
+          </div>
+        </div>
+        <div className='way'><div className='white'>2.30</div><div className='white'>2.30</div><div className='white'>4.05</div></div>
+        <div className='way'><AiFillLock/><AiFillLock/><AiFillLock/></div>
+        <div className='way'><div className='yell'>2.5</div><div className='white'>2.39</div><div className='white'>1.51</div></div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
